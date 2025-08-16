af6411f0c5e5bd878fd2a660cf9129573a68caf4
Dmitrii Liubanevich <dlyubanevich@gmail.com>
2025-08-16 21:07:18 +0300

    Add headers for request

--- src/websocket1c.rs
+++ src/websocket1c.rs
@@ -1,14 +1,17 @@
-
-use tokio::net::TcpStream;
-use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
-use addin1c::{name, AddinResult, MethodInfo, Methods, PropInfo, SimpleAddin, Variant};
+use addin1c::{name, AddinResult, CStr1C, MethodInfo, Methods, PropInfo, SimpleAddin, Variant};
 use futures_util::{
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
 };
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 use std::{sync::Arc, time::Duration};
+use tokio::net::TcpStream;
 use tokio::runtime::Runtime;
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::http::{Request, Uri},
+    MaybeTlsStream, WebSocketStream,
+};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -23,17 +26,25 @@ impl WebSocket1CAddIn {
         Ok(Self::default())
     }
 
-    fn connect(&mut self, address: &mut Variant, return_value: &mut Variant) -> AddinResult {
+    fn connect(
+        &mut self,
+        address: &mut Variant,
+        json_headers: &mut Variant,
+        return_value: &mut Variant,
+    ) -> AddinResult {
         self.runtime.clone().block_on(async {
-            let address = address.get_string()?;
-            let (stream, _) = connect_async(&address).await?;
+            let request_data =
+                RequestData::try_new(address.get_string()?, json_headers.get_string()?)?;
+            let request = Request::try_from(request_data)?;
+            let (stream, _) = connect_async(request)
+                .await
+                .map_err(|error| format!("{error}"))?;
             let (sender, receiver) = stream.split();
             self.websocket = Some(WebSocketConnection { sender, receiver });
             return_value.set_bool(true);
             Ok(())
         })
     }
-
     fn send(&mut self, message: &mut Variant, return_value: &mut Variant) -> AddinResult {
         self.runtime.clone().block_on(async {
             let message = message.get_string()?;
@@ -42,12 +53,14 @@ impl WebSocket1CAddIn {
                 Some(websocket) => {
                     websocket
                         .sender
-                        .send(tokio_tungstenite::tungstenite::Message::Text(message.into()))
+                        .send(tokio_tungstenite::tungstenite::Message::Text(
+                            message.into(),
+                        ))
                         .await?;
                     return_value.set_bool(true);
                     Ok(())
                 }
-            }       
+            }
         })
     }
 
@@ -66,15 +79,15 @@ impl WebSocket1CAddIn {
                         Err(_) | Ok(None) => {
                             return_value.set_str1c("")?;
                             Ok(())
-                        },
-                        Ok(Some(result)) => {                     
+                        }
+                        Ok(Some(result)) => {
                             let message = result?.to_text()?.to_owned();
                             return_value.set_str1c(message)?;
                             Ok(())
-                        },
+                        }
                     }
                 }
-            }        
+            }
         })
     }
     fn disconnect(&mut self, return_value: &mut Variant) -> AddinResult {
@@ -82,7 +95,7 @@ impl WebSocket1CAddIn {
         return_value.set_bool(true);
         Ok(())
     }
-    fn version(&mut self, return_value: &mut Variant) -> AddinResult {  
+    fn version(&mut self, return_value: &mut Variant) -> AddinResult {
         return_value.set_str1c(VERSION.to_owned())?;
         Ok(())
     }
@@ -97,7 +110,7 @@ impl WebSocket1CAddIn {
 }
 
 impl SimpleAddin for WebSocket1CAddIn {
-    fn name() -> &'static [u16] {
+    fn name() -> &'static CStr1C {
         name!("WebSocket1CAddIn")
     }
     fn save_error(&mut self, err: Option<Box<dyn Error>>) {
@@ -107,7 +120,7 @@ impl SimpleAddin for WebSocket1CAddIn {
         &[
             MethodInfo {
                 name: name!("Подключиться"),
-                method: Methods::Method1(Self::connect),
+                method: Methods::Method2(Self::connect),
             },
             MethodInfo {
                 name: name!("ОтправитьСообщение"),
@@ -153,4 +166,66 @@ struct WebSocketConnection {
         tokio_tungstenite::tungstenite::Message,
     >,
     receiver: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+}
+
+#[derive(Default)]
+struct RequestData {
+    address: String,
+    headers: HashMap<String, String>,
+}
+
+impl RequestData {
+    fn try_new(address: String, json_headers: String) -> Result<Self, String> {
+        let headers = if !json_headers.is_empty() {
+            serde_json::from_str::<HashMap<String, serde_json::Value>>(&json_headers)
+                .map_err(|error| error.to_string())?
+                .into_iter()
+                .map(|(key, value)| {
+                    let value = match value {
+                        serde_json::Value::Null => "".to_owned(),
+                        serde_json::Value::Bool(bool) => bool.to_string(),
+                        serde_json::Value::Number(number) => number.to_string(),
+                        serde_json::Value::String(str) => str,
+                        serde_json::Value::Array(_) => "".to_owned(),
+                        serde_json::Value::Object(_) => "".to_owned(),
+                    };
+                    (key, value)
+                })
+                .collect()
+        } else {
+            HashMap::default()
+        };
+
+        Ok(RequestData { address, headers })
+    }
+}
+
+impl TryFrom<RequestData> for Request<()> {
+    type Error = String;
+
+    fn try_from(data: RequestData) -> Result<Self, Self::Error> {
+        let uri = data.address.parse::<Uri>().map_err(|err| err.to_string())?;
+        let authority = uri
+            .authority()
+            .ok_or("No host name in the URL".to_string())?
+            .as_str();
+        let host = authority
+            .find('@')
+            .map(|idx| authority.split_at(idx + 1).1)
+            .unwrap_or_else(|| authority);
+
+        let websocket_key = tokio_tungstenite::tungstenite::handshake::client::generate_key();
+
+        let mut request_builder = Request::get(data.address.as_str())
+            .header("Host", host)
+            .header("Connection", "Upgrade")
+            .header("Upgrade", "websocket")
+            .header("Sec-WebSocket-Version", "13")
+            .header("Sec-WebSocket-Key", websocket_key);
+
+        for (key, value) in data.headers.iter() {
+            request_builder = request_builder.header(key.as_str(), value.as_str());
+        }
+        request_builder.body(()).map_err(|error| error.to_string())
+    }
 }
